285628e82940a78d9a890f8b27139dfd8747a48b
halotitee <moreseed01@gmail.com>
2017-05-23 18:08:54 -0400

    ready to merge upstream

--- src/component/pages/DetailedPlantPage.js
+++ src/component/pages/DetailedPlantPage.js
@@ -137,58 +137,67 @@ export default class DetailedPlantPage extends Component {
 
     return(
       <div className='DetailedPlantPage'>
-        <div className='DetailedPlantPage-content'>
-          <div className='row'>
-            <div className='DetailedPlantPage-info col-large-4 col-medium-4 col-small-12'>
-              <h1>{ nickname }</h1>
-              <h4>{ name } </h4>
-              <h4>{ description }</h4>
-              <div className={ this.state[util.HUMIDITY] ? "DetailedPlantPage-info-hum-toggled" : "DetailedPlantPage-info-box" }
-                   onClick={ () => {this._toggleDataSet(util.HUMIDITY)} }>
-                <FontAwesome className='hum-icon' name='tint' size='3x' style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/>
-                <p>{ currentHum } %</p>
-              </div>
-              <div
-                className={ this.state[util.TEMPERATURE] ? "DetailedPlantPage-info-temp-toggled" : "DetailedPlantPage-info-box" }
-                onClick={ () => this._toggleDataSet(util.TEMPERATURE)}>
-                <FontAwesome className='temp-icon' name='thermometer-three-quarters' size='3x' style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/>
-                <p>{ currentTemp } &deg;C</p>
+          <div className='DetailedPlantPage-content'>
+            <div className='row'>
+              <div className='DetailedPlantPage-info col-large-4 col-medium-4 col-small-12'>
+                <h1>{ nickname }</h1>
+                <h4>{ name } </h4>
+                <h4>{ description }</h4>
+                <div
+                  className={ this.state[util.HUMIDITY] ? "DetailedPlantPage-info-hum-toggled" : "DetailedPlantPage-info-box" }
+                  onClick={ () => {
+                    this._toggleDataSet(util.HUMIDITY)
+                  } }>
+                  <FontAwesome className='hum-icon' name='tint' size='3x'
+                               style={{textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}/>
+                  <p>{ currentHum } %</p>
+                </div>
+                <div
+                  className={ this.state[util.TEMPERATURE] ? "DetailedPlantPage-info-temp-toggled" : "DetailedPlantPage-info-box" }
+                  onClick={ () => this._toggleDataSet(util.TEMPERATURE)}>
+                  <FontAwesome className='temp-icon' name='thermometer-three-quarters' size='3x'
+                               style={{textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}/>
+                  <p>{ currentTemp } &deg;C</p>
 
+                </div>
+                <div
+                  className={ this.state[util.LUX] ? "DetailedPlantPage-info-lux-toggled" : "DetailedPlantPage-info-box" }
+                  onClick={ () => this._toggleDataSet(util.LUX)}>
+                  <FontAwesome className='lux-icon' name='sun-o' size='3x'
+                               style={{textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}/>
+                  <p>{ currentLux }lux</p>
+                </div>
+                <div
+                  className={ this.state[util.FERTILITY] ? "DetailedPlantPage-info-ph-toggled" : "DetailedPlantPage-info-box" }
+                  onClick={ () => this._toggleDataSet(util.FERTILITY)}>
+                  <FontAwesome className='fertility-icon' name='flask' size='3x'
+                               style={{textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}/>
+                  <p>{ currentFertility } f* </p>
+                </div>
               </div>
-              <div className={ this.state[util.LUX] ? "DetailedPlantPage-info-lux-toggled" : "DetailedPlantPage-info-box" }
-                   onClick={ () => this._toggleDataSet(util.LUX)}>
-                <FontAwesome className='lux-icon' name='sun-o' size='3x' style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/>
-                <p>{ currentLux }lux</p>
+
+
+              { this.state.loading &&
+              <div className='DetailedPlantPage-chart col-large-7 col-medium-6 col-small-12'>
+                <div className="spinner">
+                  <div className="bounce1"></div>
+                  <div className="bounce2"></div>
+                  <div className="bounce3"></div>
+                </div>
               </div>
-              <div className={ this.state[util.FERTILITY] ? "DetailedPlantPage-info-ph-toggled" : "DetailedPlantPage-info-box" }
-                   onClick={ () => this._toggleDataSet(util.FERTILITY)}>
-                <FontAwesome className='fertility-icon' name='flask' size='3x' style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/>
-                <p>{ currentFertility } f* </p>
+              }
+
+              { !this.state.loading &&
+              <div className='DetailedPlantPage-chart col-large-7 col-medium-6 col-small-12'>
+                <Chart data={ chartStuff.data } options={ chartStuff.options }/>
+                <div className='DetailedPlantPage-options'>
+                  <h4 onClick={ () => this.setState({period: 'day'})}> DAY </h4>
+                  <h4 onClick={ () => this.setState({period: 'week'})}> WEEK </h4>
+                </div>
               </div>
+              }
             </div>
-
-
-            { this.state.loading &&
-            <div className='DetailedPlantPage-chart col-large-7 col-medium-6 col-small-12'>
-              <div className="spinner">
-                <div className="bounce1"></div>
-                <div className="bounce2"></div>
-                <div className="bounce3"></div>
-              </div>
-            </div>
-            }
-
-            { !this.state.loading &&
-            <div className='DetailedPlantPage-chart col-large-7 col-medium-6 col-small-12'>
-              <Chart data={ chartStuff.data } options={ chartStuff.options }/>
-              <div className='DetailedPlantPage-options'>
-                <h4 onClick={ () => this.setState({period: 'day'})}> DAY </h4>
-                <h4 onClick={ () => this.setState({period: 'week'})}> WEEK </h4>
-              </div>
-            </div>
-            }
           </div>
-        </div>
       </div>
     );
   }
